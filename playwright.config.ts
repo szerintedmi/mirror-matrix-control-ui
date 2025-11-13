eb95f0cce4793822fe4b494b863b8ce5633808ef
szerintedmi <szerintedmi@gmail.com>
2025-11-13 23:23:29 +0000

    reduce playwright workers for non CI e2e tests too to avoid  timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     fullyParallel: true,
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 2 : undefined,
+    workers: process.env.CI ? 2 : 2,
     reporter: process.env.CI
         ? [['github'], ['html', { open: 'never' }]]
         : [['list'], ['html', { open: 'never' }]],
